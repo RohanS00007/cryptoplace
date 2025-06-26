41965d53865c0c22ac1ed3e7db747390bc433197
Rohan Singh <rohansingh70112@gmail.com>
2025-06-26 09:56:27 +0530

    updated

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || "/cryptoplace"
+  base: "/"
 })
